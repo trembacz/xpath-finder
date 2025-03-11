431dd70b4f13d9288020753fbfac6d754038acf6
Tomasz Rembacz <4508357+trembacz@users.noreply.github.com>
2025-03-11 15:02:19 +0100

    migrate to manifest v3

--- background.js
+++ background.js
@@ -1,24 +1,35 @@
 const browserAppData = this.browser || this.chrome;
 const tabs = {};
-const inspectFile = 'inspect.js';
-const activeIcon = 'active-64.png';
-const defaultIcon = 'default-64.png';
+const inspectFile = "inspect.js";
+const activeIcon = "active-64.png";
+const defaultIcon = "default-64.png";
 
 const inspect = {
   toggleActivate: (id, type, icon) => {
     this.id = id;
-    browserAppData.tabs.executeScript(id, { file: inspectFile }, () => { browserAppData.tabs.sendMessage(id, { action: type }); });
-    browserAppData.browserAction.setIcon({ tabId: id, path: { 19: 'icons/' + icon } });
+    browserAppData.scripting.executeScript(
+      {
+        target: { tabId: id },
+        files: [inspectFile]
+      },
+      () => {
+        browserAppData.tabs.sendMessage(id, { action: type });
+      }
+    );
+
+    browserAppData.action.setIcon({ tabId: id, path: { 19: "icons/" + icon } });
   }
 };
 
 function isSupportedProtocolAndFileType(urlString) {
-  if (!urlString) { return false; }
-  const supportedProtocols = ['https:', 'http:', 'file:'];
-  const notSupportedFiles = ['xml', 'pdf', 'rss'];
-  const extension = urlString.split('.').pop().split(/\#|\?/)[0];
-  const url = document.createElement('a');
-  url.href = urlString;
+  if (!urlString) {
+    return false;
+  }
+  const url = new URL(urlString);
+  const supportedProtocols = ["https:", "http:", "file:"];
+  const notSupportedFiles = ["xml", "pdf", "rss"];
+  const extension = url.href.split(".").pop().split(/\#|\?/)[0];
+
   return supportedProtocols.indexOf(url.protocol) !== -1 && notSupportedFiles.indexOf(extension) === -1;
 }
 
@@ -26,9 +37,9 @@ function toggle(tab) {
   if (isSupportedProtocolAndFileType(tab.url)) {
     if (!tabs[tab.id]) {
       tabs[tab.id] = Object.create(inspect);
-      inspect.toggleActivate(tab.id, 'activate', activeIcon);
+      inspect.toggleActivate(tab.id, "activate", activeIcon);
     } else {
-      inspect.toggleActivate(tab.id, 'deactivate', defaultIcon);
+      inspect.toggleActivate(tab.id, "deactivate", defaultIcon);
       for (const tabId in tabs) {
         if (tabId == tab.id) delete tabs[tabId];
       }
@@ -42,7 +53,7 @@ function deactivateItem(tab) {
       for (const tabId in tabs) {
         if (tabId == tab[0].id) {
           delete tabs[tabId];
-          inspect.toggleActivate(tab[0].id, 'deactivate', defaultIcon);
+          inspect.toggleActivate(tab[0].id, "deactivate", defaultIcon);
         }
       }
     }
@@ -50,11 +61,13 @@ function deactivateItem(tab) {
 }
 
 function getActiveTab() {
-  browserAppData.tabs.query({ active: true, currentWindow: true }, tab => { deactivateItem(tab); });
+  browserAppData.tabs.query({ active: true, currentWindow: true }, tab => {
+    deactivateItem(tab);
+  });
 }
 
 browserAppData.commands.onCommand.addListener(command => {
-  if (command === 'toggle-xpath') {
+  if (command === "toggle-xpath") {
     browserAppData.tabs.query({ active: true, currentWindow: true }, tab => {
       toggle(tab[0]);
     });
@@ -62,4 +75,4 @@ browserAppData.commands.onCommand.addListener(command => {
 });
 
 browserAppData.tabs.onUpdated.addListener(getActiveTab);
-browserAppData.browserAction.onClicked.addListener(toggle);
+browserAppData.action.onClicked.addListener(toggle);
--- options.js
+++ options.js
@@ -1,15 +1,16 @@
 const browserAppData = this.browser || this.chrome;
-const shortcutCommand = 'toggle-xpath';
-const updateAvailable = (typeof browserAppData.commands.update !== 'undefined') ? true : false;
+const shortcutCommand = "toggle-xpath";
+const updateAvailable = typeof browserAppData.commands.update !== "undefined" ? true : false;
 const isMac = navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i) ? true : false;
-const shortCutKeys = isMac ? 'Command+Shift+' : 'Ctrl+Shift+';
-const shortCutLabels = isMac ? 'CMD+SHIFT+' : 'CTRL+SHIFT+';
+const shortCutKeys = isMac ? "Command+Shift+" : "Ctrl+Shift+";
+const shortCutLabels = isMac ? "CMD+SHIFT+" : "CTRL+SHIFT+";
 
 async function updateShortcut() {
-  updateAvailable && await browserAppData.commands.update({
-    name: shortcutCommand,
-    shortcut: shortCutKeys + document.querySelector('#shortcut').value
-  });
+  updateAvailable &&
+    (await browserAppData.commands.update({
+      name: shortcutCommand,
+      shortcut: shortCutKeys + document.querySelector("#shortcut").value
+    }));
 }
 
 async function resetShortcut() {
@@ -18,7 +19,7 @@ async function resetShortcut() {
     const commands = await browserAppData.commands.getAll();
     for (const command of commands) {
       if (command.name === shortcutCommand) {
-        document.querySelector('#shortcut').value = command.shortcut.substr(-1);
+        document.querySelector("#shortcut").value = command.shortcut.substr(-1);
       }
     }
     saveOptions();
@@ -30,51 +31,57 @@ function shortcutKeyField(event) {
 }
 
 function saveOptions(e) {
-  browserAppData.storage.local.set({
-    inspector: document.querySelector('#inspector').checked,
-    clipboard: document.querySelector('#copy').checked,
-    shortid: document.querySelector('#shortid').checked,
-    position: document.querySelector('#position').value,
-    shortcut: document.querySelector('#shortcut').value
-  }, () => {
-    const status = document.querySelector('.status');
-    status.textContent = 'Options saved.';
-    updateAvailable && updateShortcut();
-    setTimeout(() => {
-      status.textContent = '';
-    }, 1000);
-  });
+  browserAppData.storage.local.set(
+    {
+      inspector: document.querySelector("#inspector").checked,
+      clipboard: document.querySelector("#copy").checked,
+      shortid: document.querySelector("#shortid").checked,
+      position: document.querySelector("#position").value,
+      shortcut: document.querySelector("#shortcut").value
+    },
+    () => {
+      const status = document.querySelector(".status");
+      status.textContent = "Options saved.";
+      updateAvailable && updateShortcut();
+      setTimeout(() => {
+        status.textContent = "";
+      }, 1000);
+    }
+  );
   e && e.preventDefault();
 }
 
 function restoreOptions() {
-  browserAppData.storage.local.get({
-    inspector: true,
-    clipboard: true,
-    shortid: true,
-    position: 'bl',
-    shortcut: 'U'
-  }, items => {
-    document.querySelector('#inspector').checked = items.inspector;
-    document.querySelector('#copy').checked = items.clipboard;
-    document.querySelector('#shortid').checked = items.shortid;
-    document.querySelector('#position').value = items.position;
-    document.querySelector('#shortcut').value = items.shortcut;
-  });
+  browserAppData.storage.local.get(
+    {
+      inspector: true,
+      clipboard: true,
+      shortid: true,
+      position: "bl",
+      shortcut: "U"
+    },
+    items => {
+      document.querySelector("#inspector").checked = items.inspector;
+      document.querySelector("#copy").checked = items.clipboard;
+      document.querySelector("#shortid").checked = items.shortid;
+      document.querySelector("#position").value = items.position;
+      document.querySelector("#shortcut").value = items.shortcut;
+    }
+  );
 }
 
 // update shortcut string in options box
-document.querySelector('.command').textContent = shortCutLabels;
+document.querySelector(".command").textContent = shortCutLabels;
 
 // check if browser support updating shortcuts
 if (updateAvailable) {
-  document.querySelector('#reset').addEventListener('click', resetShortcut);
-  document.querySelector('#shortcut').addEventListener('keyup', shortcutKeyField);
+  document.querySelector("#reset").addEventListener("click", resetShortcut);
+  document.querySelector("#shortcut").addEventListener("keyup", shortcutKeyField);
 } else {
   // remove button and disable input field
-  document.querySelector('#reset').remove();
-  document.querySelector('#shortcut').setAttribute('disabled', 'true');
+  document.querySelector("#reset").remove();
+  document.querySelector("#shortcut").setAttribute("disabled", "true");
 }
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.querySelector('form').addEventListener('submit', saveOptions);
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
